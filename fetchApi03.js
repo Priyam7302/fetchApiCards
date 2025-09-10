3e4da84a8af5c5c8b5f9d079938f711d4feb535b
Priyam7302 <priyamvada7302@gmail.com>
2025-09-10 15:11:57 +0530

    convert dollar into rupees using another api

--- fetchApi03.js
+++ fetchApi03.js
@@ -2,20 +2,32 @@ let cardMain = document.querySelector(".card-main");
 let loading = document.querySelector(".loadingPara");
 console.log(loading);
 let url = "https://ecommerce-api-8ga2.onrender.com/api/product";
+let priceUrl = "https://api.frankfurter.dev/v1/latest?base=USD&symbols=INR";
 
 async function fetchProducts() {
-  let response = await fetch(url);
-  let result = await response.json();
-  if (result.length > 0) {
-    loading.classList.remove("loadingPara");
-    loading.classList.add("show");
+  try {
+    let priceResponse = await fetch(priceUrl);
+    let priceResult = await priceResponse.json();
+    console.log(priceResult);
+
+    let usdToInr = priceResult.rates.INR;
+    console.log("1 USD = " + usdToInr + " INR");
+
+    let response = await fetch(url);
+    let result = await response.json();
+    if (result.length > 0) {
+      loading.classList.remove("loadingPara");
+      loading.classList.add("show");
+    }
+    console.log(result);
+    showProducts(result,usdToInr);
+  } catch {
+    console.log("error");
   }
-  console.log(result);
-  showProducts(result);
 }
 fetchProducts();
 
-function showProducts(r) {
+function showProducts(r,usdToInr) {
   let n = r.length;
   console.log(n);
   for (let i = 1; i <= n; i++) {
@@ -25,7 +37,8 @@ function showProducts(r) {
     let price = document.createElement("p");
     img.src = r[i].url;
     title.innerText = r[i].name;
-    price.innerText = r[i].price;
+
+    price.innerText = (r[i].price * usdToInr).toFixed(2) + "₹";
     card.append(img, title, price);
     cardMain.append(card);
   }
